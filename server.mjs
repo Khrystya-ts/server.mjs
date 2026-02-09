8f4b9f5c1ebfa2f79bc83e0a3e7197edd09ef313
Khrystya Tsymbaliuk <khrystyna.tsymbaliuk@gmail.com>
2026-02-09 20:07:32 +0200

    Update database connection string to use environment variables

--- server.mjs
+++ server.mjs
@@ -1,7 +1,7 @@
 import pg from 'pg';
 const { Client } = pg;
 const client = new Client({
-    connectionstring: 'postgresql:// neondb_owner:npg'
+    connectionstring: `postgresql://${process.env.DATABASE_NAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}?sslmode=require&channel_binding=require&uselibpqcompat=true`
 });
 
 async function getData () {
